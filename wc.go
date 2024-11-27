b37e5894603c30999c28b78fad27e750e8c7baf5
Ujas Thakkar <42888663+thakkarujas5@users.noreply.github.com>
2024-11-28 00:28:07 +0530

    add support for char, line, word count

---
+++ wc.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	argsLength := len(os.Args)
+
+	var filesLength int
+	lineCount := false
+	charCount := false
+	wordCount := false
+
+	for x, i := range os.Args {
+
+		if i == "-l" {
+			lineCount = true
+		}
+
+		if i == "-c" {
+			charCount = true
+		}
+		if i == "-w" {
+			wordCount = true
+		}
+		if i != "./wc" && i != "-l" && i != "-w" && i != "-c" {
+			filesLength = argsLength - x
+			break
+		}
+	}
+
+	files := make([]string, filesLength)
+
+	for y, x := 0, argsLength-filesLength; x < argsLength; x, y = x+1, y+1 {
+		files[y] = os.Args[x]
+	}
+
+	for _, file := range files {
+
+		content, err := os.ReadFile(file)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fileContent := string(content)
+
+		lines := strings.Split(fileContent, "\n")
+
+		wc := 0
+		cc := 0
+		for _, line := range lines {
+
+			words := strings.Split(line, " ")
+			wc += len(words)
+
+			for _, word := range words {
+				cc += len(word)
+			}
+		}
+
+		if lineCount {
+			fmt.Print(len(lines), " ")
+		}
+
+		if wordCount {
+			fmt.Print(wc, " ")
+		}
+
+		if charCount {
+			fmt.Print(cc, " ")
+		}
+
+		fmt.Println(file)
+	}
+
+}
